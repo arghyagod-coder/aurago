ae91596afed1567da88fe0ba31f27cea6af6c8f5
Shravan Asati <dev.shravan@protonmail.com>
2021-08-11 21:18:22 +0530

    ✨ progress bars and improved cli

--- aura.go
+++ aura.go
@@ -7,62 +7,72 @@ import (
 	"os"
 	"strings"
 
+	"github.com/schollz/progressbar/v3"
 	"github.com/thatisuday/commando"
 )
 
 const (
 	NAME    string = "aura"
-	VERSION string = "1.2.0"
+	VERSION string = "1.3.0"
 )
 
-func DownloadFile(filepath string, url string) error {
+func downloadFile(filepath, url string) error {
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
+
 	out, err := os.Create(filepath)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
-	_, err = io.Copy(out, resp.Body)
+
+	bar := progressbar.DefaultBytes(resp.ContentLength, filepath)
+
+	_, err = io.Copy(io.MultiWriter(out, bar), resp.Body)
 	return err
 }
 
 func main() {
 	fmt.Println(NAME, VERSION)
+
 	commando.
 		SetExecutableName(NAME).
 		SetVersion(VERSION).
 		SetDescription("aura is a command line utility for fetching content from the web. It is efficient and fast, being written in golang.")
+
 	commando.
 		Register(nil).
 		AddArgument("url", "The URL to download.", "").
-		// comp := strings.Split(args["url"].Value , "/")
-		AddFlag("fn", "File name where your file will be executed", commando.String, "").
+		AddFlag("filename,f", "File name where your file will be executed", commando.String, ":detect").
 		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
-			// fileUrl := "https://raw.githubusercontent.com/MatMoul/archfi/master/archfi"
-			comp := strings.Split(args["url"].Value, "/")
-			if flags["fn"].Value.(string) == "" {
-				dir := comp[len(comp)-1]
-				err := DownloadFile(dir, args["url"].Value)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println("Downloaded: " + args["url"].Value)
-			} else {
-				dir := flags["fn"].Value.(string)
-				err := DownloadFile(dir, args["url"].Value)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println("Downloaded: " + args["url"].Value)
+
+			url := args["url"].Value
+			comp := strings.Split(url, "/")
+			filename, e := flags["filename"].GetString()
+			if e != nil {
+				filename = comp[len(comp)-1]
 			}
-		},
 
-		// err := DownloadFile(dir , args["url"].Value )
+			if flags["filename"].Value.(string) == ":detect" {
+				filename = comp[len(comp)-1]
+				err := downloadFile(filename, url)
+				if err != nil {
+					fmt.Println("Unable to download file:", err)
+				}
+				fmt.Println("Downloaded: " + url)
 
-		)
+			} else {
+				err := downloadFile(filename, url)
+				if err != nil {
+					fmt.Println("Unable to download file:", err)
+				}
+
+				fmt.Println("Downloaded: " + url)
+			}
+		})
+
 	commando.Parse(nil)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
-module aura.go
+module github.com/arghyagod-coder/aura
 
 go 1.16
 
-require github.com/thatisuday/commando v1.0.4 // indirect
+require (
+	github.com/schollz/progressbar/v3 v3.8.2
+	github.com/thatisuday/commando v1.0.4
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+)
